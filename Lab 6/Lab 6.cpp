49ed25ed14e25dcd1353e0fe312931c3c66485b3
Flame1000-7 <kolipzpzol123@gmail.com>
2024-12-28 14:01:36 +0300

    Finished. I change my  own calculating in the date function into chrono calculate.

--- Lab 6/Lab 6.cpp
+++ Lab 6/Lab 6.cpp
@@ -3,93 +3,36 @@
 #include <fstream>
 #include <sstream>
 #include <clocale>
-#include <stdlib.h>
-#include <stdio.h>
+#include <chrono>
+#include <vector>
+#include <set>
 
 using namespace std;
+using namespace std::chrono;
+using namespace std::chrono_literals;
 
 int interations = 0;
 int listNum = 1;
 
-void splitDate(string str,int& day, int& month, int& year) {
-    sscanf_s(str.c_str(), "%d.%d.%d", &day, &month, &year);
+struct VocRecords {
+    string fullName;
+    sys_days startDate;
+    sys_days endDate;
+    int days;
+
+};
+
+vector<VocRecords> records;
+
+sys_days splitDate(const string& str) {
+    int d, m, y;
+    sscanf_s(str.c_str(), "%d.%d.%d", &d, &m, &y);
+    return year(y) / month(m) / day(d);
 }
 
-class DateCalculator {
-public:
-    DateCalculator(int day, int month, int year) : day(day), month(month), year(year) {}
-    static int countDate(const DateCalculator start, const DateCalculator end) {
-
-        if (start.year > end.year || (start.year == end.year && start.month > end.month) || (start.year == end.year && start.month == end.month && start.day > end.day) ) {
-
-            return -1;
-
-        }
-
-        int totalDays = 0;
-        int daysInPeriod = 0;
-        if (start.month == end.month && start.year == end.year) {
-            totalDays += end.day - start.day;
-        }
-        else if (start.month != end.month && start.year == end.year) {
-            if (start.day == end.day) {
-                totalDays += daysInMonth(start.month, start.year);
-            }
-            else {
-                totalDays += daysInMonth(start.month, start.year) + (end.day - start.day);
-            }
-        }
-        else if (start.year != end.year) {
-            if (start.month == end.month) {
-                totalDays += (end.day - start.day) + (isLeapYear(start.year) ? 366 : 365) + ((isLeapYear(end.year) ? 366 : 365) - 365);
-            }
-            else {
-                for (int i = __min(start.month, end.month); i < __max(end.month,start.month); i++) {
-                    daysInPeriod += daysInMonth(i, start.year);
-                }
-                if (start.day == end.day) {
-                    if (start.month > end.month) {
-                        totalDays += (isLeapYear(start.year) ? 366 : 365) + ((isLeapYear(end.year) ? 366 : 365) - 365) - daysInPeriod;
-                    }
-                    else {
-                        totalDays += (isLeapYear(start.year) ? 366 : 365) + ((isLeapYear(end.year) ? 366 : 365) - 365) + daysInPeriod;
-                    }
-                }
-                else {
-                    if (start.month > end.month) {
-                        totalDays += (isLeapYear(start.year) ? 366 : 365) + (end.day - start.day) - daysInPeriod;
-                    }
-                    else {
-                        totalDays += (isLeapYear(start.year) ? 366 : 365) + (end.day - start.day) + daysInPeriod;
-                    }
-                    
-                }
-            }
-        }
-
-        return totalDays;
-    }
-
-private:
-
-    int day, month, year;
-
-    static bool isLeapYear(int year) {
-
-        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-
-    }
-    static int daysInMonth(int month, int year) {
-
-        switch (month) {
-        case 1: case 3: case 5: case 7: case 8: case 10: case 12: return 31;
-        case 4: case 6: case 9: case 11: return 30;
-        case 2: return isLeapYear(year) ? 29 : 28;
-        default: return 0;
-
-        }
-    }
-};
+int calcDays(sys_days date1, sys_days date2) {
+    return (date2 - date1).count();
+}
 
 void Flush() {
     setlocale(LC_ALL, "ru");
@@ -105,9 +48,6 @@ void Flush() {
 
 void inputInfoIntoFile() {
     setlocale(LC_ALL, "ru");
-
-    int Sd, Sm, Sy;
-    int Ed, Em, Ey;
     int countDays;
 
     string fulName;
@@ -123,13 +63,7 @@ void inputInfoIntoFile() {
     cout << "Введите дату выхода из отпуска(ДД.ММ.ГГГГ): ";
     getline(cin, endDate);
 
-    splitDate(startDate, Sd, Sm, Sy);
-    splitDate(endDate, Ed, Em, Ey);
-
-    DateCalculator sDate(Sd, Sm, Sy);
-    DateCalculator eDate(Ed, Em, Ey);
-
-    countDays = DateCalculator::countDate(sDate, eDate);
+    countDays = calcDays(splitDate(startDate), splitDate(endDate));
 
     ofstream outFile("C:\\Users\\Kolip\\Desktop\\Base.txt", ios::app);
     if (!outFile) {
@@ -149,60 +83,56 @@ void inputInfoIntoFile() {
     cout << endl;
 }
 
-bool isDateInRange(string start, string end,string voidDate) {
-    int Sd, Sm, Sy;
-    int Ed, Em, Ey;
-    int Vd, Vm, Vy;
-
-    splitDate(start, Sd, Sm, Sy);
-    splitDate(end, Ed, Em, Ey);
-    splitDate(voidDate, Vd, Vm, Vy);
-
-    DateCalculator sDate(Sd, Sm, Sy);
-    DateCalculator eDate(Ed, Em, Ey);
-    DateCalculator vDate(Vd, Vm, Vy);
-
-    if (DateCalculator::countDate(sDate, eDate) != -1) {
-        return (DateCalculator::countDate(sDate, eDate) <= DateCalculator::countDate(sDate, vDate));
-    }
-
+bool isDateInRange(sys_days VoidDate, sys_days startDate, sys_days endDate) {
+    return VoidDate >= startDate && VoidDate <= endDate;
 }
 
 void getinfoFromBase() {
     setlocale(LC_ALL, "ru");
 
     string voidDate;
-    string fullName;
-    string startDate;
-    string vocationLen;
-    string endDate;
 
     cout << "Введите дату для запроса(ДД.ММ.ГГГГ): "; cin >> voidDate;
-    
+    cout << endl;
     if (voidDate != "") {
         ifstream fromFile("C:\\Users\\Kolip\\Desktop\\Base.txt");
-
         if (!fromFile.is_open()) {
             cerr << "Error";
             return;
         }
-
+        VocRecords currentRecord;
         string line;
         while (getline(fromFile, line)) {
             if (line.find("ФИО: ") != string::npos) {
-                fullName = line.substr(line.find(":") + 2);
-                getline(fromFile, line);
-                startDate = line.substr(line.find(":") + 2);
-                getline(fromFile, line);
-                endDate = line.substr(line.find(":") + 2);
-                getline(fromFile, line);
-                vocationLen = line.substr(line.find(":") + 2);
-                if (isDateInRange(startDate, endDate, voidDate)) {
-                    cout << fullName << " - Находиться в отпуске" << endl;
+                currentRecord.fullName = line.substr(line.find(":") + 2);
+            }
+            else if (line.find("Дата ухода в отпуск: ") != string::npos) {
+                currentRecord.startDate = splitDate(line.substr(line.find(":") + 2));
+            }
+            else if (line.find("Дата выхода из отпуска: ") != string::npos) {
+                currentRecord.endDate = splitDate(line.substr(line.find(":") + 2));
+            }
+            records.push_back(currentRecord);
+        }
+        fromFile.close();
+        sys_days inputDate = splitDate(voidDate);
+        cout << "Сотрудники в отпуске на " << voidDate << ":" << endl;
+        cout << endl;
+        set <string> printedNames;
+        bool found = false;
+        for (const auto& record : records) {
+            if (isDateInRange(inputDate, record.startDate, record.endDate)) {
+                if (printedNames.find(record.fullName) == printedNames.end()) {
+                    cout << record.fullName << endl;
+                    cout << endl;
+                    printedNames.insert(record.fullName);
+                    found = true;
                 }
             }
         }
-        fromFile.close();
+        if (!found) {
+            cout << "Никто не в отпуске на эту дату." << endl;
+        }
     }
 }
 
